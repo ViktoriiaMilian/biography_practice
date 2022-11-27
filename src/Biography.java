fec93daa8cb58aac79bfb213592a537889e9dd29
Viktoriia Milian <milianvika@gmail.com>
2022-11-27 16:34:35 -0600

    added biography project

--- src/Biography.java
+++ src/Biography.java
@@ -1,26 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SplittableRandom;
+
 public class Biography {
     public static void main(String[] args) {
 
         /**
          * WORK ON BOOK AND AUTHOR CLASSES FIRST
-        This will be our actual program that we define author and his books
-        In this program we will write the biography of Stefan Zweig, an Austrian novelist.
+         This will be our actual program that we define author and his books
+         In this program we will write the biography of Stefan Zweig, an Austrian novelist.
 
 
          Write a program that will get information from user and
          -Print information for the favorite author
          -Print information of the books of favorite author
 
-        Full name = Stefan Zweig
-        County = Austria
-        Is still alive: No (28 November 1881 – 22 February 1942)
-        Some of his books as listed below:
+         Full name = Stefan Zweig
+         County = Austria
+         Is still alive: No (28 November 1881 – 22 February 1942)
+         Some of his books as listed below:
 
-        BookName                            Genre           TotalPage
-        Amok                                tale            189
-        The Royal Game                      novella         53
-        24 Hours in the Life of a Woman     novella         80
+         BookName                            Genre           TotalPage
+         Amok                                tale            189
+         The Royal Game                      novella         53
+         24 Hours in the Life of a Woman     novella         80
          */
+
 
         /*
         Expected result:
@@ -33,5 +38,13 @@ public class Biography {
 
         //YOUR CODE HERE
 
+        Author author = Author.newAuthor();
+        System.out.println(author);
+        System.out.println("Author's books are as listed below:");
+        author.listOfBooks.forEach(System.out::println);
+
+
+
     }
+
 }
